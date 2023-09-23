5ca92b7d15083a972f4956bff6bddecf14dc7ef8
Gabor Sos <gabor.sos1994@gmail.com>
2023-09-23 22:34:24 +0200

    maven project files added, .gitignore file expanded

---
+++ backend/src/test/java/com/findyourjob/FindYourJob/FindYourJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findyourjob.FindYourJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
