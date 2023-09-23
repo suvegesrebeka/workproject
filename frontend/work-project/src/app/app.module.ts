bb8f103a6dea9d4170454f70fd268af61516eca4
srebii <suvegesrebeka@gmail.com>
2023-09-23 23:22:01 +0200

    #4 - add material package

--- frontend/work-project/src/app/app.module.ts
+++ frontend/work-project/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModule } from 'src/app/components/pages/login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    LoginModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- frontend/work-project/src/app/components/pages/login/login.module.ts
+++ frontend/work-project/src/app/components/pages/login/login.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from 'src/app/components/pages/login/login.component';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule],
   exports: [LoginComponent],
 })
 export class LoginModule {}
